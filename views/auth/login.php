2647ce58c3ff96735a5564f36968d39d13e5c7e0
mauriciojesus30 <130005383+mauriciojesus30@users.noreply.github.com>
2023-10-20 18:49:31 -0400

    Creando la vista de login

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,26 @@
-<h2> <?php echo $titulo;?> </h2>
+<main class="auth">
+  <h2 class="auth__heading"><?php echo $titulo; ?></h2>
+  <p class="texto">Inicia sesion en DevWebCamp</p>
+
+<form action="" class="formulario">
+
+  <div class="formulario__campo">
+    <label for="email" class="formulario__label">Correo Electronico</label>
+    <input type="email" class="formulario__input" name="email" id="email" placeholder="Tu Correo Electronico">
+  </div>
+
+  <div class="formulario__campo">
+    <label for="password" class="formulario__label">Contraseña</label>
+    <input type="password" class="formulario__input" name="password" id="password" placeholder="Tu contraseña">
+  </div>
+
+  <input type="submit" class="formulario__submit" value="Iniciar Sesion">
+
+</form>
+
+<div class="acciones">
+  <a href="/registro" class="acciones__enlace">¿Aun no tienes una cuenta? Obtener una</a>
+  <a href="/olvide" class="acciones__enlace">¿Olvidaste tu contraseña?</a>
+</div>
+
+</main>
