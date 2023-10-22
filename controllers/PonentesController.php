da3fe4eab3e107b6ccd5d9036b15b7c4cd0fa5ce
pr0d1gy00 <calitoalejandro184@gmail.com>
2023-10-22 18:48:21 -0400

    finalizando ponentes, aun no funciona

--- controllers/PonentesController.php
+++ controllers/PonentesController.php
@@ -2,8 +2,9 @@
 
 namespace Controllers;
 
+use Model\Ponente;
 use MVC\Router;
-
+use Intervention\Image\ImageManagerStatic as Image;
 class PonentesController {
 
 
@@ -16,10 +17,56 @@ class PonentesController {
   public static function crear(Router $router) {
 
     $alertas = [];
+    $ponente = new Ponente;
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+      //leer imagen
+
+      if(!empty($_FILES['imagen']['tmp_name'])){
+        $carpeta_imagenes = '../public/img/speakers';
+
+        //crear la carpeta si no existe
+        if(!is_dir($carpeta_imagenes)){
+          mkdir($carpeta_imagenes,0777,true);
+        }
+
+        $imagen_png = Image::make($_FILES['imagen']['tpm_name'])->fit(800,800)->encode('png',80);
+        $imagen_webp = Image::make($_FILES['imagen']['tpm_name'])->fit(800,800)->encode('webp',80);
+
+        $nombre_imagen = md5(uniqid(rand(),true));
+
+        $_POST['imagen'] = $nombre_imagen;
+        
+        $_POST['redes'] =json_encode($_POST['redes'], JSON_UNESCAPED_SLASHES);
+
+      
+        $ponente->sincronizar($_POST);
+  
+        $alertas = $ponente->validar();
+      }
+     
+
+      //guardar registro
+      if(empty($alertas)){
+        //guardar imagenes
+        $imagen_png->save($carpeta_imagenes . '/' . $nombre_imagen . ".png");
+        $imagen_webp->save($carpeta_imagenes . '/' . $nombre_imagen . ".webp");
+
+
+        //guardar en BD
+        $resultado = $ponente->guardar();
+        if($resultado){
+          header('Location: /admin/ponentes');
+        }
+
+      }
+
+    }
 
     $router->render('admin/ponentes/crear', [
       'titulo' => 'Registrar Ponente',
-      'alertas' => $alertas
+      'alertas' => $alertas,
+      'ponente' => $ponente
     ]);
   }
 }
